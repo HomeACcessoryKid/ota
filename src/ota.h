578d1d2f8e5c731bd7fae61096779750e0145547
HomeACcessoryKid <homeaccessorykid@gmail.com>
2018-12-23 16:24:57 +0100

    0.2.0 switch over to LCM repository
    This is a one way ticket to start using the life-cycle-manager
    repository
    when it executes, it will set the ota-main routine to the latest LCM
    version

--- src/ota.h
+++ src/ota.h
@@ -6,11 +6,12 @@
  #error You must set OTAVERSION=x.y.z of the ota code to match github version tag x.y.z
 #endif
 #define OTAREPO  "HomeACcessoryKid/ota"
+#define LCMREPO  "HomeACcessoryKid/life-cycle-manager"
 #define MAINFILE "otamain.bin"
 #define BOOTFILE "otaboot.bin"
 #define CERTFILE "certs.sector"
-#define HOLDOFF_MULTIPLIER 5    //more like 20  -> 20s,400 (~6min),8000 (~2h),160000 (~2days)
-#define HOLDOFF_MAX 120         //more like 604800 (1 week)
+#define HOLDOFF_MULTIPLIER 20    //more like 20  -> 20s,400 (~6min),8000 (~2h),160000 (~2days)
+#define HOLDOFF_MAX 604800       //more like 604800 (1 week)
 
 #define SECTORSIZE 4096
 #define HIGHERCERTSECTOR 0xF6000
